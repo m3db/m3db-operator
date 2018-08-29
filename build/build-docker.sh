d4229037d9150a0c409bb9a3980d51815f00fa77
Paul Schooss <pschooss@uber.com>
2018-08-29 17:27:45 -0400

    Update documentation with minikub

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -11,9 +11,11 @@ fi
 
 : ${IMAGE:?"Need to set IMAGE, e.g. gcr.io/<repo>/<your>-operator"}
 
-GITSHA="$(git rev-parse head)"
+GITSHA="$(git rev-parse HEAD)"
 
 echo "building container ${IMAGE}:${GITSHA}..."
 docker build -t "${IMAGE}:${GITSHA}" -f Dockerfile .
+docker tag "${IMAGE}:${GITSHA}" "${IMAGE}:latest"
 docker tag "${IMAGE}:${GITSHA}" "${IMAGE}:${GITSHA}"
 docker push "${IMAGE}:${GITSHA}"
+docker push "${IMAGE}:latest"
