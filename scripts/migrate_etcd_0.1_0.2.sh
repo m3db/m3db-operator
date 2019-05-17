37d5987a1ccf8ad98e8e6c828c2fa57b88f984de
Matt Schallert <mattschallert@gmail.com>
2019-05-17 16:11:49 -0400

    make script work on OSX
    Co-authored-by: Arnaud Meyer <arnaud.meyer@idealo.de>

--- scripts/migrate_etcd_0.1_0.2.sh
+++ scripts/migrate_etcd_0.1_0.2.sh
@@ -2,6 +2,14 @@
 
 set -exo pipefail
 
+TMP=$(mktemp)
+
+function cleanup() {
+  rm -f "$TMP"
+}
+
+trap cleanup EXIT
+
 if [[ "$1" == "-h" || -z "$ETCD_NS" || -z "$ETCD_POD" || -z "$M3DB_NS" || -z "$M3DB_CLUSTER" ]]; then
   echo "Script for migrating etcd data from m3db-operator 0.1 -> 0.2"
   echo "Usage: ETCD_NS=<namespace> ETCD_POD=<pod> M3DB_NS=<namespace> M3DB_CLUSTER=<cluster_name> ./migrate_etcd_0.1_0.2.sh"
@@ -19,5 +27,17 @@ fi
 ENV="$NS/$CLUSTER"
 echo "Copying namespace and placement data from env=default_env to env=$ENV"
 
-kubectl exec -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl get --print-value-only _sd.placement/default_env/m3db | head -c-1 | kubectl exec -i -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl put "_sd.placement/$ENV/m3db"
-kubectl exec -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl get --print-value-only _kv/default_env/m3db.node.namespaces | head -c-1 | kubectl exec -i -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl put "_kv/$ENV/m3db.node.namespaces"
+
+# Put placement bytes (includes trailing newline) into TMP
+kubectl exec -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl get --print-value-only _sd.placement/default_env/m3db > "$TMP"
+
+# Trim newline in a cross-platform (OSX + Linux) manner and put in new key
+N=$(<"$TMP" wc -c)
+N=$((N-1))
+head -c "$N" "$TMP" | kubectl exec -i -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl put "_sd.placement/$ENV/m3db"
+
+# Repeat for namespaces
+kubectl exec -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl get --print-value-only _kv/default_env/m3db.node.namespaces > "$TMP"
+N=$(<"$TMP" wc -c)
+N=$((N-1))
+head -c "$N" "$TMP" | kubectl exec -i -n "$ETCD_NS" "$ETCD_POD" -- env ETCDCTL_API=3 etcdctl put "_kv/$ENV/m3db.node.namespaces"
