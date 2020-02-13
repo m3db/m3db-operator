5d84e87b57a2a851cde0469dbab4ca9d48c15a60
Matt Schallert <matt@chronosphere.io>
2020-02-12 23:35:49 -0500

    [ci] Ensure integration tests always compile
    We might not be running them, but want to ensure the test code itself
    still compiles.

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -7,4 +7,4 @@ export PACKAGE=github.com/m3db/m3db-operator
 echo "--- :git: Updating git submodules"
 git submodule update --init --recursive
 echo "--- Running unit tests"
-make clean-all test-ci-unit lint metalint bins test-all-gen
+make clean-all test-ci-unit lint metalint bins test-all-gen build-integration
