5a66d156b6f7bc472ef650a8e9399d6278693f9c
Matt Schallert <mattschallert@gmail.com>
2021-02-11 18:06:56 +0000

    WIP

--- scripts/kind-create-cluster.sh
+++ scripts/kind-create-cluster.sh
@@ -8,18 +8,18 @@ KUBE_VERSION=${KUBE_VERSION:-v1.15.7}
 CLUSTER_NAME=kind
 L_UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 
-mkdir -p "$HOME/bin"
+# mkdir -p "$HOME/bin"
 
-if [[ ! -x "$HOME/bin/kind" || "$BUILDKITE" == "true" ]]; then
-  curl -sL -o "$HOME/bin/kind" "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-${L_UNAME}-amd64"
-fi
+# if [[ ! -x "$HOME/bin/kind" || "$BUILDKITE" == "true" ]]; then
+#   curl -sL -o "$HOME/bin/kind" "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-${L_UNAME}-amd64"
+# fi
 
-if [[ ! -x "$HOME/bin/kubectl" || "$BUILDKITE" == "true" ]]; then
-  curl -sL -o "$HOME/bin/kubectl" "https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/${L_UNAME}/amd64/kubectl"
-fi
+# if [[ ! -x "$HOME/bin/kubectl" || "$BUILDKITE" == "true" ]]; then
+#   curl -sL -o "$HOME/bin/kubectl" "https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/${L_UNAME}/amd64/kubectl"
+# fi
 
-chmod +x "$HOME/bin/kind" "$HOME/bin/kubectl"
-export PATH="$HOME/bin:$PATH"
+# chmod +x "$HOME/bin/kind" "$HOME/bin/kubectl"
+# export PATH="$HOME/bin:$PATH"
 
 echo "--- :kubernetes: Deleting existing kind clusters"
 kind get clusters -q | while read -r CLUSTER; do
@@ -59,7 +59,7 @@ nodes:
         node-labels: "failure-domain.beta.kubernetes.io/zone=us-east1-d"
 EOF
 
-kind create cluster --image "kindest/node:$KUBE_VERSION" --name "$CLUSTER_NAME" --config cluster.yaml
+kind create cluster --image "kindest/node:v1.15.12@sha256:67181f94f0b3072fb56509107b380e38c55e23bf60e6f052fbd8052d26052fb5" --name "$CLUSTER_NAME" --config cluster.yaml
 
 kubectl get nodes
 kubectl cluster-info
